8b4d4c8554a27fcf96e97100cbbef3c3488b0a6c
Colson Xu <colson.xu@gmail.com>
2024-02-25 05:14:21 -0500

    added dependencies

--- embedding.py
+++ embedding.py
@@ -1,5 +1,4 @@
 import re
-import pandas as pd
 from openai import OpenAI
 from pinecone import Pinecone, ServerlessSpec
 from tqdm.auto import tqdm
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import numpy as np
 import tempfile
 from transcriptor import transcribe
 from yt_caption import get_caption_from_youtube
